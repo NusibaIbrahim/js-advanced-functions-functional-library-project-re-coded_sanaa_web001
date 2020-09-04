7b253b385de54cf04e6949723316c13388911f3e
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-04 22:34:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-  each: function(collection, callback) {
-    const items = (collection instanceof Array)? collection : Object.values(collection);
+ each: function() {
+    each: function(collection, iteratee) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
-    for(const item of items){
-      callback(item);
-    }
+      for (let idx = 0; idx < newCollection.length; idx++)
+        iteratee(newCollection[idx])
 
-    return collection;
-
-  },
+      return collection
+    },
 
     map: function() {
 
