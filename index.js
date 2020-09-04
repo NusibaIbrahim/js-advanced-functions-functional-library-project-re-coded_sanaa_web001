9862eb8c8f1bb337f7b8d8ceccdf69eaf2120cb6
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-04 22:43:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
+    each: function() {
     each: function(collection, iteratee) {
       const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
-  
+
       for (let idx = 0; idx < newCollection.length; idx++)
         iteratee(newCollection[idx])
-  
+
       return collection
     },
 
-     map: function(collection, iteratee) {
+    map: function(collection, iteratee) {
       if (!(collection instanceof Array))
         collection = Object.values(collection)
 
@@ -25,23 +26,24 @@ const fi = (function() {
       return newArr
     },
 
+
 		reduce: function(c = [], callback = () => {}, acc) {
-  		let collection = c.slice(0)
-  
-  		if (!acc) {
-  			acc = collection[0]
-  			collection = collection.slice(1)
-  		}
-  
-  		let len = collection.length;
-  
-  		for (let i = 0; i < len; i++) {
-  			acc = callback(acc, collection[i], collection)
-  		}
-  		return acc;
-	 }, 
-	 
-	 find: function(collection, predicate) {
+			let collection = c.slice(0)
+
+			if (!acc) {
+				acc = collection[0]
+				collection = collection.slice(1)
+			}
+
+			let len = collection.length;
+
+			for (let i = 0; i < len; i++) {
+				acc = callback(acc, collection[i], collection)
+			}
+			return acc;
+		},
+
+    find: function(collection, predicate) {
       if (!(collection instanceof Array))
         collection = Object.values(collection)
 
@@ -50,7 +52,8 @@ const fi = (function() {
 
       return undefined
     },
- filter: function(collection, predicate) {
+
+    filter: function(collection, predicate) {
       if (!(collection instanceof Array))
         collection = Object.values(collection)
 
