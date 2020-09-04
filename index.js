5f6e875bcc3e5d98861e2a299267d1e36ca812a2
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-04 22:39:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,6 +67,10 @@ const fi = (function() {
       return (collection instanceof Array) ? collection.length : Object.keys(collection).length
     },
 
+first: function(collection, stop=false) {
+      return (stop) ? collection.slice(0, stop) : collection[0]
+    },
+
 
 
     functions: function() {
